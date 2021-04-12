0032f3012724b0bc3db7cb02d82adf653dbcc3d6
Interfiber <hunterstasonis@icloud.com>
2021-04-12 17:39:21 -0400

    :fire: Remove API key selector

--- src/main.rs
+++ src/main.rs
@@ -5,45 +5,7 @@ use kas::widget::{Label, TextButton, Window, EditBox};
 use kas::class::HasStr;
 // Modules
 mod plugins;
-mod keys;
 
-// Ask keys will ask the user to enter their CurseForge api key
-fn ask_keys() -> Box<dyn kas::Window> {
-    // Construct a row widget, with state and children
-    let content = make_widget! {
-        #[layout(column)]
-        #[widget(config=noauto)]
-        struct {
-            #[widget] display: impl HasString = Label::new("Enter CurseForge API key:".to_string()),
-            #[widget] editor: EditBox = EditBox::new(""),
-            #[widget(handler = save_keys)] _ = TextButton::new_msg("Confirm Keys", ()),
-        }
-        impl {
-            fn save_keys(&mut self, _mgr: &mut Manager, _: ()) -> Response<VoidMsg> {
-                println!(":: saving api keys to .volt_keys");
-                std::fs::write(".volt_keys", self.editor.get_str());
-                println!(":: keys added, exiting volt...");
-                std::process::exit(1);
-            }
-        }
-        impl kas::WidgetConfig {
-            fn configure(&mut self, mgr: &mut Manager) {
-                mgr.enable_alt_bypass(true);
-            }
-        }
-        impl Handler {
-            type Msg = VoidMsg;
-            fn handle(&mut self, _mgr: &mut Manager, event: Event) -> Response<VoidMsg> {
-                println!("{:#?}", event);
-                return Response::Unhandled
-            }
-        }
-    };
-
-    let mut window = Window::new("Volt - Setup API keys", content);
-    window.set_restrict_dimensions(true, true);
-    Box::new(window)
-}
 // Browse plugins will allow the user to browse curseforge plugins
 fn browse_plugins() -> Box<dyn kas::Window> {
     // Construct a row widget, with state and children
@@ -79,25 +41,14 @@ fn browse_plugins() -> Box<dyn kas::Window> {
             }
         }
     };
-    let mut window = Window::new("Volt - Browse Plugins", content);
+    let window = Window::new("Volt - Browse Plugins", content);
     Box::new(window)
 }
 fn main() -> Result<(), kas_wgpu::Error> {
     println!(":: volt has started");
-    println!(":: checking for CurseForge api keys...");
-    if !std::path::Path::new(".volt_keys").exists(){
-        println!(":: keys not present.");
-        let theme = kas_theme::ShadedTheme::new()
-            .with_font_size(18.0);
-        kas_wgpu::Toolkit::new(theme)?
-            .with_boxed(ask_keys())?
-            .run()
-    }else {
-        println!(":: keys present.");
-        let theme = kas_theme::ShadedTheme::new()
-            .with_font_size(18.0);
-        kas_wgpu::Toolkit::new(theme)?
-            .with_boxed(browse_plugins())?
-            .run()
-    }
+    let theme = kas_theme::ShadedTheme::new()
+        .with_font_size(18.0);
+    kas_wgpu::Toolkit::new(theme)?
+        .with_boxed(browse_plugins())?
+        .run()
 }
